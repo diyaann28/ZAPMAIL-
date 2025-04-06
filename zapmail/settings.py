c71f667a29e081ae24275e8dd624f7006bd6a0ea
Your Name <you@example.com>
2025-04-06 14:04:13 +0530

    ok

--- zapmail/settings.py
+++ zapmail/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'saniaissac@gmail.com'
-EMAIL_HOST_PASSWORD = 'xole kwiu pihf biin'
+EMAIL_HOST_USER = 'zapmailemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'kngf remv rhtv wbkn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
